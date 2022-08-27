36e2bf39fdbd748400b928585041c1d1d11c244d
915marco <915marco@gmail.com>
2022-08-26 23:16:04 -0500

    piano sounds starting to be added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Howdy")
+
+function playSound() {
+    new Audio('24-piano-keys/key05.mp3').play()
+}
+
+const pianoKeys = document.querySelectorAll('.key')
+pianoKeys.forEach(pianoKey => {
+    pianoKey.addEventListener("click", playSound )
+})
